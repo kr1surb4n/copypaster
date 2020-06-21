d50629a170ae9c35d2b9da4158006d4ecac88380
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2020-06-21 14:57:39 +0200

    icon buffer test

--- copypaster/signal_bus.py
+++ copypaster/signal_bus.py
@@ -1,5 +1,21 @@
 from copypaster.register import register_instance
 
+"""
+SignalBus
+
+is an Event Subscribe-Emit object
+
+You need an object that implements function `on_<EVENT_NAME>`. 
+This object is subscribed by passing: <EVENT_NAME> and the object itself. 
+
+To emit an event you need SignalBus object and pass to emit function
+an event name and arguments. Ths will call every subscribed function.
+
+
+
+I run `emit` function and this runs some functions. I don't know what happens.
+"""
+
 
 @register_instance
 class SignalBus:
