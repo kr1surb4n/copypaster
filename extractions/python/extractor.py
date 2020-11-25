b2641ae679de4ff89f590a8d7775a58790b80056
Kris <kris@whereibend.space>
2020-11-25 22:33:35 +0100

    worked on extractor,

--- extractions/python/extractor.py
+++ extractions/python/extractor.py
@@ -2,21 +2,36 @@ import glob
 from collections import namedtuple
 from os.path import dirname, splitext, isdir
 import yaml
+from copy import copy
+import logging as l
+from lxml import etree
+from functools import partial
 
 p = print
 
+slices = lambda sausage: sausage.split("/")
+
+convert_to_text = partial(etree.tostring, method="text", encoding="UTF-8")
+
+def read(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+# CONS
 HTMLS = 'html'
 OUTPUT = 'output'
 
-
 PATTERN = HTMLS + "/**/*.html"
 
+# representation of the extracted data
 REGISTER = {}
 
-from copy import copy
 
+# placeholder for button data
 Button = namedtuple('Button', ['name', 'value', 'info', 'click_count'], defaults=["", 0])
 
+# placeholder for deck data
 class ButtonCard:
     def __init__(self, name, category):
         self.buttons = {}
@@ -32,13 +47,9 @@ class ButtonCard:
         }
 
 
-def extract_content(filename):
+def extract_content(file):
     """ 
-        open file.
-        find all objects with class: section
-        
-        for each section
-            section_name = find_first_header in section
+           
             catgory = [name, filename]
 
             extract all dt
@@ -54,15 +65,44 @@ def extract_content(filename):
             yield section_name, ButtonCard(buttons = dt + code)
     """
 
+    _, _, filename = slices(file)
+
+    l.info("Reading file to tree")
+    try:
+        content_tree = etree.HTML(read(file))
+    except Exception as e:
+        l.debug(e)
+        raise e
+
+    # for each section
+    for deck in content_tree.xpath("//*[@class='section']"):
+
+        #  section_name = find_first_header in section
+        title = ""
+        for h in deck.iter('h1', 'h2', 'h3', 'h4'):
+            title = h.text
+            break
+
+        definitions = [convert_to_text(dt) for dt in deck.xpath('//dt')]
+
+        codes = [convert_to_text(code) for code in deck.xpath("//div[@class='highlight']/pre")]
+
+        l.info(title)
+        l.info(definitions)
+        l.info(codes)
+
+        print(codes)
+
+        return
 
     buttons = [Button(filename, filename)._asdict()]
 
     return buttons
 
-def add_to_register(file, entry):
+def add_to_register(file):
+    l.info("Begining content extraction")
 
-    _, folder, filename = entry
-
+    _, folder, filename = slices(file)
 
     name = splitext(filename)[0]
 
@@ -72,32 +112,41 @@ def add_to_register(file, entry):
     if name not in REGISTER[folder]:
         REGISTER[folder][name] = {}
 
-    content = extract_content(filename)
+    content = extract_content(file)
 
+    # TODO this will go somewhere else soon
     card = ButtonCard(name, [name])
     card.buttons = content
 
+    # TODO this also will go away
     REGISTER[folder][name] = card._to_dict_()
 
-for file in glob.glob(PATTERN, recursive=True):
-    if isdir(file):     # we dont do stuff to folders
-        continue
+
+
+if __name__ == "__main__":
+    l.info("Running main")
     
-    entry = file.split("/")
 
-    if len(entry) == 2: # we don't like files in root folder
-        continue
+    for file in glob.glob(PATTERN, recursive=True):
+        if isdir(file):     # we dont do stuff to folders
+            continue
 
-    add_to_register(file, entry)
+        l.info("Working with file: ", file)
+
+        if len(slices(file)) == 2: # we don't like files in root folder
+            l.info("Oh no! I don't like this file! It's in root!")
+            continue
+
+        add_to_register(file)
 
 
 
-"""
-    Output parsed stuff
-"""
+    """
+        Output parsed stuff
+    """
 
-#p(REGISTER)
+    #p(REGISTER)
 
-dumps = yaml.dump(REGISTER)
+    dumps = yaml.dump(REGISTER)
 
-p(dumps)
+    # p(dumps)
