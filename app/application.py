fcb0bb172eeb03d253e3d8581912cfdf914495d2
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2021-06-29 18:23:15 +0200

    Application has test

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 from app import log, CURRENT_DIR
+import app
 from app.signal_bus import emit, event
 from app.register import Register as __, register_instance
 
@@ -33,3 +34,36 @@ class Application(Gtk.Application):
         log.info("Goodbye! Application terminated.")
 
 application = Application()
+
+
+def test_application_object():
+    # prepare the event handlers
+    from app.signal_bus import signal_bus
+
+    activated = False
+    started = False
+
+    def activate_app():
+        nonlocal activated
+        activated = True
+
+    def start_app():
+        nonlocal started
+        started = True
+
+    signal_bus.subscribe(event.activate_app, activate_app)
+    signal_bus.subscribe(event.start_app, start_app)
+
+    assert isinstance(application, Gtk.Application)
+
+    # run and test if handlers where run    
+    from time import sleep
+    import threading
+    def run():
+        application.run()
+    tested_thread = threading.Thread(target=run)
+    tested_thread.start()
+    sleep(1)
+    assert started and activated
+
+
