b1a5a566e77ca6327cb14528a8e115e211e8a3d9
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2021-06-29 17:57:34 +0200

    State has tests

--- app/state.py
+++ app/state.py
@@ -9,6 +9,10 @@ class State:
 
     def __init__(self, states):
         log.info("State object initialized")
+        
+        assert isinstance(states, list)
+        assert len(states) > 0
+
         self.states = states
         self.state = INIT
 
@@ -18,12 +22,38 @@ class State:
         except AttributeError:
             ...
         
+        assert isinstance(name, str)
         log.info(f"Switching to state {name.upper()}")
         if name.upper() in self.states:
-            self.state = name.upper()            
+            self.state = name.upper()
+        else:
+            raise AttributeError(f"No state: {name.upper()}")            
 
     def current(self):
         return self.state
 
     def is_(self, state):
         return state.upper() == self.state
+
+
+def test_state():
+    import pytest
+
+    with pytest.raises(TypeError):
+        state = State()
+    
+    with pytest.raises(AssertionError):
+        state = State("a")
+
+    with pytest.raises(AssertionError):
+        state = State([])
+    
+    state = State([INIT, NORMAL])
+    assert state
+    assert state.current() == INIT
+
+    with pytest.raises(AttributeError):
+        state.bad
+    
+    state.normal
+    assert state.is_(NORMAL)
